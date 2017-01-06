b2a352934917672b8865094906be1b791749ffe6
Ahmed Agabani <agabani@live.com>
2017-01-06 18:41:57 +0000

    midi project

--- midi/Properties/AssemblyInfo.cs
+++ midi/Properties/AssemblyInfo.cs
@@ -4,11 +4,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("cli")]
+[assembly: AssemblyTitle("midi")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("cli")]
+[assembly: AssemblyProduct("midi")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -19,7 +19,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("e59d68a8-c463-4b79-bd04-ab949e182457")]
+[assembly: Guid("c38d76f1-aa88-47c0-a529-be6b053107d2")]
 
 // Version information for an assembly consists of the following four values:
 //
