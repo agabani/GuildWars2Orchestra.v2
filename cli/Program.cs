04e5d720893136fce9f43fc9bf6beb319e16a58e
Ahmed Agabani <agabani@live.com>
2017-01-14 20:10:23 +0000

    program

--- cli/Program.cs
+++ cli/Program.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using guildwars;
 using midi;
 using music;
@@ -11,69 +10,12 @@ namespace cli
     {
         private static void Main(string[] args)
         {
+            var sheet = InputFromMidi();
             Info();
-            //Guildwars();
-            Midi();
-            //Generate();
+            OutputToWave(sheet);
         }
 
-        private static void Info()
-        {
-            const string path = "prelude.mid";
-
-            var midiInfo = new MidiInfo(path);
-
-            Console.WriteLine($"Path:           {path}");
-            Console.WriteLine($"Tempo:          {midiInfo.Tempo}");
-            Console.WriteLine($"Time Signature: {midiInfo.TimeSignature}");
-            Console.WriteLine($"Tracks:         {midiInfo.TrackInfos.Count}");
-
-            foreach (var keyValuePair in midiInfo.TrackInfos)
-            {
-                Console.WriteLine($"Track: {keyValuePair.Key}");
-                Console.WriteLine($"    Number of Notes: {keyValuePair.Value.NumberOfNotes}");
-                if (keyValuePair.Value.LowestTone != null)
-                {
-                    Console.WriteLine($"    Lowest Tone:     {keyValuePair.Value.LowestTone.Note}.{keyValuePair.Value.LowestTone.Octave}");
-                }
-                if (keyValuePair.Value.HighestTone != null)
-                {
-                    Console.WriteLine($"    Highest Tone:    {keyValuePair.Value.HighestTone.Note}.{keyValuePair.Value.HighestTone.Octave}");
-                }
-            }
-        }
-
-        private static void Guildwars()
-        {
-            var sheet = new MidiReader().Read("Pokemon Red Version  Pokemon Blue Version - Pokemon Center.mid");
-
-            var eventQueue = new EventQueue();
-
-            var player = new Player(eventQueue, new Controller(new Keyboard()));
-
-            player.Start();
-
-            var jukebox = new Jukebox(eventQueue);
-
-            jukebox.Play(sheet);
-        }
-
-        private static void Midi()
-        {
-            var sheet = new MidiReader().Read("prelude.mid");
-
-            var generator = new TimedSignalGenerator(new AudioTokenConvertor(sheet.Tempo));
-
-            var audioSamples = generator.Generate(sheet);
-
-            var wavePacker = new WavePacker();
-            using (var memoryStream = wavePacker.Pack(audioSamples))
-            {
-                wavePacker.Write("midi.wav", memoryStream);
-            }
-        }
-
-        private static void Generate()
+        private static Sheet InputFromHardcode()
         {
             var length = new Length {Extended = false, Fraction = Fraction.Quater};
 
@@ -113,19 +55,67 @@ namespace cli
                 new Token {Length = length, Tone = new Tone {IsRest = false, Note = Note.B, Octave = Octave.Second}}
             };
 
-            var convertor = new AudioTokenConvertor(200);
-            var signalGenerator = new SignalGenerator();
+            return new Sheet
+            {
+                Tempo = 200,
+                Tokens = new[] {tokens.ToArray()}
+            };
+        }
+
+        private static Sheet InputFromMidi()
+        {
+            return new MidiReader().Read("prelude.mid");
+        }
+
+        private static void Info()
+        {
+            const string path = "prelude.mid";
+
+            var midiInfo = new MidiInfo(path);
+
+            Console.WriteLine($"Path:           {path}");
+            Console.WriteLine($"Tempo:          {midiInfo.Tempo}");
+            Console.WriteLine($"Time Signature: {midiInfo.TimeSignature}");
+            Console.WriteLine($"Tracks:         {midiInfo.TrackInfos.Count}");
+
+            foreach (var keyValuePair in midiInfo.TrackInfos)
+            {
+                Console.WriteLine($"Track: {keyValuePair.Key}");
+                Console.WriteLine($"    Number of Notes: {keyValuePair.Value.NumberOfNotes}");
+                if (keyValuePair.Value.LowestTone != null)
+                {
+                    Console.WriteLine($"    Lowest Tone:     {keyValuePair.Value.LowestTone.Note}.{keyValuePair.Value.LowestTone.Octave}");
+                }
+                if (keyValuePair.Value.HighestTone != null)
+                {
+                    Console.WriteLine($"    Highest Tone:    {keyValuePair.Value.HighestTone.Note}.{keyValuePair.Value.HighestTone.Octave}");
+                }
+            }
+        }
+
+        private static void OutputToGuildwars(Sheet sheet)
+        {
+            var eventQueue = new EventQueue();
+
+            var player = new Player(eventQueue, new Controller(new Keyboard()));
+
+            player.Start();
+
+            var jukebox = new Jukebox(eventQueue);
+
+            jukebox.Play(sheet);
+        }
+
+        private static void OutputToWave(Sheet sheet)
+        {
+            var generator = new TimedSignalGenerator(new AudioTokenConvertor(sheet.Tempo));
+
+            var audioSamples = generator.Generate(sheet);
+
             var wavePacker = new WavePacker();
-
-            var audioSamples = tokens
-                .Select(token => convertor.Convert(token))
-                .Select(audioToken => signalGenerator.GenerateSamples(audioToken))
-                .SelectMany(audioTokenSamples => audioTokenSamples)
-                .ToArray();
-
             using (var memoryStream = wavePacker.Pack(audioSamples))
             {
-                wavePacker.Write("gen.wav", memoryStream);
+                wavePacker.Write("output.wav", memoryStream);
             }
         }
     }
